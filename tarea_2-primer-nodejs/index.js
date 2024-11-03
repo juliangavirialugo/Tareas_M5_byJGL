0f051941d424f92d4700b12b9efe1619fa38ea96
Julianga <jrgavirialugo@gmail.com>
2024-11-02 22:09:33 -0500

    tarea 2 libreria chance

--- tarea_2-primer-nodejs/index.js
+++ tarea_2-primer-nodejs/index.js
@@ -1,1 +1,23 @@
-console.log('hola mundo');
+import Chance from 'chance';
+    
+const chance = new Chance();
+
+// Generar datos aleatorios
+const nombre = chance.name();
+const correo = chance.email();
+const fechaNacimiento = chance.birthday();
+const telefono = chance.phone();
+const parrafo = chance.sentence({ words: 10 });
+const edad = chance.age();
+const favoriteAniaml = chance.animal()
+
+// Imprimir los datos aleatorios generados
+console.log("Datos aleatorios generados:");
+console.log("Nombre:", nombre);
+console.log("Correo electrónico:", correo);
+console.log("Fecha de nacimiento:", fechaNacimiento.toLocaleDateString());
+console.log("Teléfono:", telefono);
+console.log('texto :',parrafo);
+console.log('edad :', edad);
+console.log('aniaml favorito: ', favoriteAniaml);
+
