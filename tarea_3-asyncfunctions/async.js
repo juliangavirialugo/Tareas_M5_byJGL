26e9a38d28206ea7c4488ba7f9b772a11cef6fdb
Julianga <jrgavirialugo@gmail.com>
2024-11-07 12:47:37 -0500

    Se realiza la tarea numero 3

---
+++ tarea_3-asyncfunctions/async.js
@@ -0,0 +1,18 @@
+
+/*=============================================
+=            Coded By Juliangade        =
+Tarea numero 3. Async Await
+=============================================*/
+
+const esperarSegundos = async (tiempo) => {
+    const time = tiempo*1000//se crea la const time para añadir el tiempo que utilizara setimeout
+    setTimeout(() => {
+        console.time('tiempo')
+        console.log('Se ha ejecutado con exito!')
+        console.timeEnd('tiempo');
+        
+    }, time);
+}
+
+export default esperarSegundos;
+//se exporta la funcion esperar segundos para que pueda utilizarse en los demas archivos
--- tarea_3-asyncfunctions/tarea3.js
+++ tarea_3-asyncfunctions/tarea3.js
@@ -1,1 +1,10 @@
-console.log('Hola');
+
+/*=============================================
+=            Coded By Juliangade        =
+Tarea numero 3. Async Await
+=============================================*/
+
+import esperarSegundos from './async.js';//se importa esperar segundos de el acrhivo asyn.js
+
+console.log(await esperarSegundos(1));
+//en la linea 3 se llama la funcion esperar segundos y se le asigna el valor en segundos que quiere que se demoren en ejecutar la funcion
