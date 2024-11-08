0be039f72d4205e37542074e0ba9c162026e1e50
Julianga <jrgavirialugo@gmail.com>
2024-11-08 10:18:45 -0500

    Se a;ade la funcionalidad de crear un archivo nuevo con los numeros pares

--- tarea_4-CRUD_archivos/find.js
+++ tarea_4-CRUD_archivos/find.js
@@ -1,8 +1,4 @@
-
-
 // const numberString =['2','3','5']
-
-
 import * as fs from 'fs';
 const archivo = './numero.txt'
 
@@ -11,12 +7,24 @@ fs.readFile(archivo, { encoding: 'utf-8' }, (error, data) => {
         console.error('Error lectura: ', error);
         return;
     }
-
     // Convertimos el contenido de data en un array de números
     const numeros = data.split('\n').map(num => parseInt(num, 10));
-
-    // Filtramos los números pares
+    // con filter filtramos los números pares
     const numPares = numeros.filter(num => num % 2 === 0);
-
+    //Luego imprimimos los numeros pares 
     console.log(`Los numeros pares del archivo: ${archivo} son: `, numPares);
+    
+
+    const contenido2 = numPares.join('\n');
+
+fs.writeFile('./numerosPares.txt',contenido2, (error)=>{
+    if (error) {
+                console.error('Error',error)
+                 return
+         }
+           
+            console.log('Archivo de numeros pares creado con exito!')
+})
+
 });
+
