9717218e6d193e2de6e49972c23a5f1c9b35cb67
Julianga <jrgavirialugo@gmail.com>
2024-11-17 21:51:51 -0500

    tarea numero 6 realizada

--- tarea_6-Servidor-node/server.js
+++ tarea_6-Servidor-node/server.js
@@ -1,5 +1,6 @@
 //Tarea numero 6 por JGL
 import http from 'http';
+import { stringify } from 'querystring';
 const port = 3002
 const products = [
     { id: 1, name: 'Laptop', price: 999.99, category: 'Electronics' },
@@ -10,48 +11,52 @@ const products = [
 const server = http.createServer((req, res)=>{
     switch (req.url) {
         case '/':
-            res.write('este es el incio')
+            res.writeHead(200)
+            res.write('este es el home')
             res.end()
             break;
-        case '/productos':
-            if (req.method ==='GET') {
-                res.writeHead(200,{"Content-Type":"JSON"} )//lo convierte en tipo JSON de una
-                res.write(products)
+        case '/products':
+            if (req.method === 'GET') {
+                res.writeHead(201, { "Content-Type": "JSON" })
+                res.write(JSON.stringify(products))
                 res.end()
                 break;
             }
-            if (req.method ==='POST') {
-                let body = ''
-                req.on('data', (chunk) =>{   
-                    body += chunk
-
-                })
-                req.on('end', () =>{
+            if (req.method === 'POST') {
+                let body = '';
+                req.on('data', (chunk) => {
+                    body += chunk.toString();
+                });
+                
+               
+                req.on('end', () => {
                     
-                    // const {title, author, year} = JSON.parse(body)
-                    // if (!title || !author || !year) {
-                    //     res.writeHead(400,'error 400')
-                    //     res.write('faltan datos ')
-                    //     res.end()
-                    //     return;
-                    // }
-                    // crear un archivo txt con los datos que nos envia el usuario
-                    res.writeHead(201,)
-                        res.write('libro creado ')
+                    const { id, price, name, category } = JSON.parse(body)
+                    if(!id || !price|| !name || !category){
+                        res.writeHead(400)
+                        res.write('faltan datos')
                         res.end()
+                        return;
+                    }
+                    products.push({ id, name, price, category });
+
+                        res.writeHead(201, { "Content-Type": "text/plain" });
+                        res.write('Producto creado');
+                        res.end();
+
+                   
                 })
-                break;
             }
-           
+
+            break;
+
+
         default:
-            res.writeHead(404,'error 404')
-            res.write('error 404')
-            res.end()
-       
+            res.writeHead(404)
+            res.write('Error en los datos de ingreso')
+        res.end()
+            break;
     }
-    
+
 })
-server.listen(port, ()=>{
-    console.log(`El servidor esta corriendo en el puerto ${port}`);
-    
-})
+server.listen(port)
